b8ec19482830aa288120fbc75f57f1673d75f6d8
wilker123 <josewilkerlucena123@gmail.com>
2021-12-08 14:47:35 -0300

    Adicionando rotação no app com RotedBox

--- gerenciador_de_boletos/lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ gerenciador_de_boletos/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -3,9 +3,10 @@ import 'package:gerenciador_de_boletos/shared/themes/app_colors.dart';
 import 'package:gerenciador_de_boletos/shared/themes/app_text_styles.dart';
 import 'package:gerenciador_de_boletos/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:gerenciador_de_boletos/shared/widgets/label_button/label_button.dart';
+import 'package:gerenciador_de_boletos/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
-  const BarcodeScannerPage({ Key? key }) : super(key: key);
+  const BarcodeScannerPage({Key? key}) : super(key: key);
 
   @override
   _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
@@ -14,51 +15,49 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text("Escaneie o código de barras do boleto", style: TextStyles.buttonBackground,),
-        leading: BackButton(color: AppColors.background),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.black,
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escaneie o código de barras do boleto",
+              style: TextStyles.buttonBackground,
             ),
+            leading: BackButton(color: AppColors.background),
           ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.transparent,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.black,
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        height: 56,
-        child: Row(
-          children: [
-            Expanded(
-              child: LabelButton(
-                label: "Inserir código do boleto", 
-                onPressed: (){},
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                ),
               ),
-            ),
-            DividerVerticalWidget(),
-            Expanded(
-              child: LabelButton(
-                label: "Adicionar da galeria", 
-                onPressed: (){},
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.transparent,
+                ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: "Inserir código do boleto",
+            primaryOnPressed: () {},
+            secundaryLabel: "Adicionqr da Galeria",
+            secundaryOnPressed: () {},
+          ),
         ),
       ),
     );
